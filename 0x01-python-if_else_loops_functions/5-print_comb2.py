67548a577a1d3eea993813e8df5f48af567a5831
Dottielaleti <laletidottie@gmail.com>
2023-05-10 04:34:17 -0700

    Print string

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num <= 99:
-        print(f"{num:02d},", end=' ')
+for x in range(100):
+    if x == 99:
+        print(x)
+    else:
+        print("{}".format('0' + str(x) if x < 10 else x), end=", ")
