920bd1f5be0009c722251ae8cff3d3bcbd78b2f8
Dottielaleti <laletidottie@gmail.com>
2023-05-14 02:22:36 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin
+def divisible_by_2(my_list=[]):
+    new_list = my_list.copy()
+    for x in range(0, len(my_list):
+            if x % 2 = 0:
+            new_list[] = True
+            else:
+            False
+            return new_list
