d288ebb3ca088298aad1e4bc69caec9448bf55da
Dottielaleti <laletidottie@gmail.com>
2023-05-29 01:48:06 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for j in range(x):
+        try:
+            print(my_list[j], end="")
+            j += 1
+        except IndexError:
+            break
+    print()
+    return j
